feafa76b870cba6ee76b7b9dab0335d6ad40f6b0
Danlin Luo <danlinluo432@gmail.com>
2024-09-10 13:56:29 -0400

    populate homepage and add links to footer

--- src/components/Header-Footer/Footer.js
+++ src/components/Header-Footer/Footer.js
@@ -4,12 +4,12 @@ function Footer () {
     return (
         <div className='Footer'>
             <span>
-                Last updated: September 7, 2024
+                Last updated: September 10, 2024
             </span>
             <ul className='Navbar-list Footer'>
-                <li>Linkedin</li>
-                <li>Github</li>
-                <li>Leetcode</li>
+                <li><a href='https://www.linkedin.com/in/danlin-luo-a69217220/' target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+                <li><a href='https://github.com/dluo6' target="_blank" rel="noopener noreferrer">Github</a></li>
+                <li><a href='https://leetcode.com/u/watermelon06/' target="_blank" rel="noopener noreferrer">Leetcode</a></li>
             </ul>
         </div>
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,44 +8,26 @@ function Home() {
             image: melon,
             imageAlt: "melon",
             text: "University",
-            description: "I am currently a senior at McGill University majoring in CS and minoring in management!"
+            description: "Despite my current minor in management, I originally majored in finance, but I pivoted after my intro computer science class opened up a new door for me."
         },
         {
             image: melon,
             imageAlt: "melon",
-            text: "University",
-            description: "I am currently a senior at McGill University majoring in CS and minoring in management!"
+            text: "Languages",
+            description: "Being born and raised in Quebec for part of my childhood, I know a bit of French. Including Mandarin, Spanish, and English, I have formally learned 4 languages."
         },
         {
             image: melon,
             imageAlt: "melon",
-            text: "University",
-            description: "I am currently a senior at McGill University majoring in CS and minoring in management!"
-        },
-        {
-            image: melon,
-            imageAlt: "melon",
-            text: "University",
-            description: "I am currently a senior at McGill University majoring in CS and minoring in management!"
-        },
-        {
-            image: melon,
-            imageAlt: "melon",
-            text: "University",
-            description: "I am currently a senior at McGill University majoring in CS and minoring in management!"
-        },
-        {
-            image: melon,
-            imageAlt: "melon",
-            text: "University",
-            description: "I am currently a senior at McGill University majoring in CS and minoring in management!"
+            text: "Hobbies",
+            description: "Outside of my professional life, you'll find me sometimes indulging in art, which is why I'm aiming to create all of the images used by my portfolio! I also enjoy other hobbies such as singing, going to the gym, and cooking."
         }
     ]
 
     const renderInfoCard = (image, imageAlt, text, description) => (
         <div className='Home-card'>
             <img src={image} alt={imageAlt}/>
-            <p>{text}</p>
+            <b>{text}</b>
             <p>{description}</p>
         </div>
     )
@@ -59,9 +41,23 @@ function Home() {
                 </div>
             </div>
             <div className='Home-about'>
-                <p>About Me</p>
+                <div className='Home-about-text'>
+                    <h1>About Me</h1>
+                    <p>Hi, I’m Danlin Luo, a computer science major at McGill University with a passion for technology, problem-solving, and creative projects. My academic journey has been enriched by hands-on experience, from implementing environmental initiatives at McGill to researching influencer marketing under Professor Hyunji So.
+                    <br /><br />
+                    I’ve developed a strong foundation in software development through my internships and projects. At Citibank, I worked as a Software Development Analyst, where I deployed front-end changes and developed internal tools.
+                    <br /><br />
+                    Outside of work, I love participating in hackathons, such as McHacks and Code.Jam(), where I’ve built solutions ranging from productivity apps to trucking notification services. Fluent in English, advanced in Mandarin, and with an intermediate grasp of Spanish, I enjoy exploring new languages and cultures in my free time.
+                    <br /><br />
+                    Feel free to connect with me if you’d like to collaborate or chat about tech innovations!
+                    </p>
+                </div>
+                <img src={melon} alt='watermelon'/>
+            </div>
+            <div className='Home-fun-facts'>
+                <h1>Fun Facts!</h1>
                 <div className='Home-info-cards'>
-                {cards.map((card) => renderInfoCard(card.image, card.imageAlt, card.text, card.description))}
+                    {cards.map((card) => renderInfoCard(card.image, card.imageAlt, card.text, card.description))}
                 </div>
             </div>
         </div>
